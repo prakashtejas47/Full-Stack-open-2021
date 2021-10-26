fd5c2c8bc638a777cf7dbdad973cd64e8367e266
prakashtejas47 <82558033+prakashtejas47@users.noreply.github.com>
2021-10-26 10:14:41 -0400

    2.19-2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,7 @@ import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import noteService from './services/notes'
 import axios from 'axios'
+import './index.css'
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
     noteService
       .getAll()
       .then(text => {
+        console.log(text)
         setPersons(text)
       })
   }, [])
@@ -28,6 +30,21 @@ const App = () => {
   const handleFilterChange = (event) => {
     setFilter_by(event.target.value)
   } 
+
+  const [message,setMessage] = useState(null)
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
   const addName = (event) =>{
     event.preventDefault()
     const found = persons.findIndex(person => person.name === newName)
@@ -35,25 +52,37 @@ const App = () => {
       if (window.confirm(newName + ' is already added to the phonebook, replace the old number with a new one?')){
         noteService
         .update(persons[found]['id'],{name: newName,number: newNumber})
-        .then(response=>response.data)
+        .then(response=>console.log(response))
+        .catch(error=>setMessage('Information of ' + newName + 'has already been removed'))
         setPersons(persons
           .map(person=>person['name']===newName ? {"name":newName, "number":newNumber,"id": person['id']} : person))
+        setNewName('')
+        setNewNumber('')
+        setMessage('Updated ' + newName)
+        setTimeout(() => {
+          setMessage(null)
+        }, 2000)
+       // setMessage('')
       }
     }
     else{
       noteService
       .create({name: newName,number: newNumber})
-      .then((added) => {
+      .then(added => {
         setPersons(persons.concat(added))
         setNewName('')
         setNewNumber('')
-        console.log(persons)
+        setMessage('Added ' + newName)
+        setTimeout(() => {
+          setMessage(null)
+        }, 2000)
       })
     }
   }
 
   return (
     <div>
+      <Notification message={message}/>
       <h2>Phonebook</h2>
       <Filter filter_by={filter_by} handleFilterChange={handleFilterChange}/>
       <h3> Add a new</h3>
