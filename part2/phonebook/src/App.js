e75446a53f959771a1dcbfae076f664f8fb5b522
prakashtejas47 <82558033+prakashtejas47@users.noreply.github.com>
2021-10-26 00:26:35 -0400

    2.15-2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect} from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import noteService from './services/notes'
 import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(text => {
+        setPersons(text)
+      })
+  }, [])
+
   const [ filter_by,setFilter_by] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
@@ -27,26 +32,25 @@ const App = () => {
     event.preventDefault()
     const found = persons.findIndex(person => person.name === newName)
     if (found!==-1){
-      window.alert(newName + ' is already added to the phonebook')
+      if (window.confirm(newName + ' is already added to the phonebook, replace the old number with a new one?')){
+        noteService
+        .update(persons[found]['id'],{name: newName,number: newNumber})
+        .then(response=>response.data)
+        setPersons(persons
+          .map(person=>person['name']===newName ? {"name":newName, "number":newNumber,"id": person['id']} : person))
+      }
     }
     else{
-      setPersons(persons.concat({name: newName,number: newNumber}))
-      setNewName('')
-      setNewNumber('')
+      noteService
+      .create({name: newName,number: newNumber})
+      .then((added) => {
+        setPersons(persons.concat(added))
+        setNewName('')
+        setNewNumber('')
+        console.log(persons)
+      })
     }
   }
-
-  const [notes, setNotes] = useState([])
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'persons')
 
   return (
     <div>
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import axios from 'axios'
 
 const Persons = (props) =>{
+    const baseUrl = 'http://localhost:3001/persons/'
+    const handleDelete = text =>event=>{
+        if (window.confirm("Delete " + text["name"] + "?")) {
+            axios
+            .delete(baseUrl+text['id'])
+            .then(response=>response.data)
+          }
+        
+        //event.preventDefault()
+        //props.setPersons(props.persons.filter(person=>person!==text))
+        //console.log(props.persons)
+    }
+    const filtered = (props.persons.filter(
+        person => person.name.toLowerCase().includes(props.filter_by.toLowerCase())))
+    const mapped = filtered.map(
+        person => 
+        <form onSubmit={handleDelete(person)}>
+        <li>{person.name} {person.number}
+        <button type="submit">delete</button>
+        </li>
+        </form>
+        )
     return(
     <ul>
-        {(props.persons.filter(
-        person => person.name.toLowerCase().includes(props.filter_by.toLowerCase()))).map(person => 
-        <li>{person.name} {person.number}</li>
-        )}
+        {mapped}
     </ul>
     )
 }
